9e26263a8637fe524158ceb3bc64b9c7f1049949
Rustem-ZR <zaidullinrustik@gmail.com>
2025-06-06 10:56:13 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,7 +1,12 @@
 public class Main {
     public static void main (String[] args) {
-        String name = ("Rustem Zaidullin");
-        System.out.println (name);
+        int income = 72000;
+        int spending = 3000;
+        System.out.println ("income = 72000");
+        System.out.println ("spending = 3000");
+        int difference = (income - spending);
+        System.out.println ("difference: ");
+        System.out.println ( difference );
 
 
     }
